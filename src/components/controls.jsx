a083d0d3df2436c6e954ebffa1f91de4b5d55f86
jeremy rajan <jeremyrajan@gmail.com>
2021-08-12 17:01:38 +0800

    remove node

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -9,8 +9,7 @@ localforage.config({
 
 const flowKey = 'example-flow';
 
-const getNodeId = () => `randomnode_${+new Date()}`;
-;
+// const getNodeId = () => `randomnode_${+new Date()}`;
 
 const Controls = ({ rfInstance, setElements }) => {
   const { transform } = useZoomPanHelper();
@@ -36,20 +35,20 @@ const Controls = ({ rfInstance, setElements }) => {
     restoreFlow();
   }, [setElements, transform]);
 
-  const onAdd = useCallback(() => {
-    const newNode = {
-      id: `random_node-${getNodeId()}`,
-      data: { label: 'Added node' },
-      position: { x: Math.random() * window.innerWidth - 100, y: Math.random() * window.innerHeight },
-    };
-    setElements((els) => els.concat(newNode));
-  }, [setElements]);
+  // const onAdd = useCallback(() => {
+  //   const newNode = {
+  //     id: `random_node-${getNodeId()}`,
+  //     data: { label: 'Added node' },
+  //     position: { x: Math.random() * window.innerWidth - 100, y: Math.random() * window.innerHeight },
+  //   };
+  //   setElements((els) => els.concat(newNode));
+  // }, [setElements]);
 
   return (
     <div className="save__controls">
       <button onClick={onSave}>save</button>
       <button onClick={onRestore}>restore</button>
-      <button onClick={onAdd}>add node</button>
+      {/* <button onClick={onAdd}>add node</button> */}
     </div>
   );
 };
